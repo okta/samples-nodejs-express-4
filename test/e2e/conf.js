056e67627b7436c0709bf0dd743612cea8db5583
Reman Child <rchild@okta.com>
2016-12-11 21:58:12 -0800

    :arrow_up: @okta/generator-samples 1.5.0
    RELATED TO OKTA-109162

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -13,51 +13,8 @@
 /* eslint import/no-unresolved:0, import/no-extraneous-dependencies:0, no-console:0 */
 /* global jasmine */
 const jasmineReporters = require('jasmine-reporters');
-const childProcess = require('child_process');
-const chalk = require('chalk');
+const startAndWaitFor = require('../../tools/start-and-wait-for');
 const samplesConfig = require('../../.samples.config.json');
-
-/**
- * Starts a child process that resolves when the given msg is found. Useful
- * when starting a standup service whose ready signal is determined by stdout.
- */
-function startAndWaitFor(cmd, args, msg, color) {
-  const name = `${cmd} ${args.join(' ')}`;
-  const normal = chalk[color || 'yellow'];
-  const bold = normal.bold.underline;
-  console.log(normal(`Running "${name}" and waiting for "${msg}"`));
-  return new Promise((resolve, reject) => {
-    const child = childProcess.spawn(cmd, args, { encoding: 'utf8' });
-
-    child.stdout.on('data', (buffer) => {
-      const data = buffer.toString('utf8');
-      console.log(normal(data));
-      if (msg && data.indexOf(msg) > -1) {
-        console.log(bold(`Found "${msg}" for "${name}", resolving`));
-        resolve(child);
-      }
-    });
-
-    child.stderr.on('data', (data) => {
-      console.log(normal(`${data}`));
-    });
-
-    child.on('close', (code) => {
-      console.log(normal(`"${name}" finished with exit code ${code}`));
-      if (code) {
-        reject();
-      } else {
-        resolve(child);
-      }
-    });
-
-    child.on('error', (err) => {
-      console.log(bold(`Failed to start ${name}`));
-      console.log(bold(err));
-      reject(err);
-    });
-  });
-}
 
 const promises = Promise.all([
   startAndWaitFor('npm', ['start'], samplesConfig.server.startSignal, 'green'),
@@ -76,9 +33,10 @@ const config = {
     }));
   },
   afterLaunch() {
-    return promises.then((childProcesses) => {
-      childProcesses.forEach(child => child.kill());
+    promises.then((childProcesses) => {
+      childProcesses.forEach(child => process.kill(-child.pid));
     });
+    return new Promise(resolve => setTimeout(() => resolve(), 1000));
   },
   specs: ['specs/*.js'],
   restartBrowserBetweenTests: false,
--- tools/mock-okta/tapes/5ec867f82f531749821bbba014ce27dd.js
+++ tools/mock-okta/tapes/5ec867f82f531749821bbba014ce27dd.js
@@ -22,22 +22,22 @@ module.exports = function (req, res) {
   res.statusCode = 200;
 
   res.setHeader("server", "Apache-Coyote/1.1");
-  res.setHeader("x-okta-request-id", "reqDPCGeydHTHWPoqYbN5aBYA");
+  res.setHeader("x-okta-request-id", "req8AkC1KNZQLCtJpKIXg7DTw");
   res.setHeader("x-rate-limit-limit", "1200");
   res.setHeader("x-rate-limit-remaining", "1199");
-  res.setHeader("x-rate-limit-reset", "1478388289");
+  res.setHeader("x-rate-limit-reset", "1481839884");
   res.setHeader("p3p", "CP=\"HONK\"");
-  res.setHeader("set-cookie", ["sid=\"\"; Expires=Thu, 01-Jan-1970 00:00:10 GMT; Path=/","JSESSIONID=70AE96C8582376C33727D5332CA5E78A; Path=/","DT=DI0szFpmcNvRFaypiGqtGoNmA; Expires=Mon, 05-Nov-2018 23:23:50 GMT; Path=/"]);
+  res.setHeader("set-cookie", ["sid=\"\"; Expires=Thu, 01-Jan-1970 00:00:10 GMT; Path=/","JSESSIONID=E6BC6FB23F18B2DE47F87D8CA1313E89; Path=/","DT=DI0L5Xl5vMSSO-UbFrul__KIg; Expires=Sat, 15-Dec-2018 22:10:24 GMT; Path=/"]);
   res.setHeader("cache-control", "no-cache, no-store");
   res.setHeader("pragma", "no-cache");
   res.setHeader("expires", "0");
   res.setHeader("content-type", "application/json;charset=UTF-8");
   res.setHeader("transfer-encoding", "chunked");
-  res.setHeader("date", "Sat, 05 Nov 2016 23:23:49 GMT");
+  res.setHeader("date", "Thu, 15 Dec 2016 22:10:24 GMT");
 
   res.setHeader("x-yakbak-tape", path.basename(__filename, ".js"));
 
-  res.write(new Buffer("eyJleHBpcmVzQXQiOiIyMDE2LTExLTA2VDAxOjIzOjUwLjAwMFoiLCJzdGF0dXMiOiJTVUNDRVNTIiwic2Vzc2lvblRva2VuIjoiMjAxMTE5UDdmTkpET1QzRDI4Z1VOLW5qS0ZHTVdMdU8tcjQ0T2tPR29PcjY3NXZkZWlqOWlnSyIsIl9lbWJlZGRlZCI6eyJ1c2VyIjp7ImlkIjoiMDB1a3o0eVpVZ2lHMExJb1IwZzMiLCJwYXNzd29yZENoYW5nZWQiOiIyMDE2LTEwLTMwVDA0OjE5OjE0LjAwMFoiLCJwcm9maWxlIjp7ImxvZ2luIjoiZ2VvcmdlQGFjbWUuY29tIiwiZmlyc3ROYW1lIjoiR2VvcmdlIiwibGFzdE5hbWUiOiJXYXNoaW5ndG9uIiwibG9jYWxlIjoiZW5fVVMiLCJ0aW1lWm9uZSI6IkFtZXJpY2EvTG9zX0FuZ2VsZXMifX19fQ==", "base64"));
+  res.write(new Buffer("eyJleHBpcmVzQXQiOiIyMDE2LTEyLTE2VDAwOjEwOjI0LjAwMFoiLCJzdGF0dXMiOiJTVUNDRVNTIiwic2Vzc2lvblRva2VuIjoiMjAxMTEzbUM0Y2RSY2dpaUhGcFctSXNlY1E2TnkxSl9HOTJaQVhrNENDanBxTDhVM1AyQlROdiIsIl9lbWJlZGRlZCI6eyJ1c2VyIjp7ImlkIjoiMDB1bGd2dGFvWjZSWlZ3T0QwZzMiLCJwYXNzd29yZENoYW5nZWQiOiIyMDE2LTEyLTA1VDAwOjMyOjM2LjAwMFoiLCJwcm9maWxlIjp7ImxvZ2luIjoiZ2VvcmdlQGFjbWUuY29tIiwiZmlyc3ROYW1lIjoiR2VvcmdlIiwibGFzdE5hbWUiOiJXYXNoaW5ndG9uIiwibG9jYWxlIjoiZW4iLCJ0aW1lWm9uZSI6IkFtZXJpY2EvTG9zX0FuZ2VsZXMifX19fQ==", "base64"));
   res.end();
 
   return __filename;
