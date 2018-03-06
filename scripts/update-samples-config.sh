b5e19bad3c8f623d7ae7500ee0bc7e2a67dcbbec
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-03-06 14:17:14 -0800

    Updating script to set the samples config

--- scripts/update-samples-config.sh
+++ scripts/update-samples-config.sh
@@ -1,5 +1,5 @@
 # The environment variables (OKTA_DOMAIN, CLIENT_ID, CLIENT_SECRET, SPA_CLIENT_ID) are set in the travis repository settings
 sed -i -- "s/{yourOktaDomain}/$OKTA_DOMAIN/g" .samples.config.json
+sed -i -- "s/\"cid\": \"{clientId}\"/\"cid\": \"$SPA_CLIENT_ID\"/g" .samples.config.json
 sed -i -- "s/{clientId}/$CLIENT_ID/g" .samples.config.json
 sed -i -- "s/{clientSecret}/$CLIENT_SECRET/g" .samples.config.json
-sed -i -- "s/\"cid\": \"{clientId}\"/\"cid\": \"$SPA_CLIENT_ID\"/g" .samples.config.json
