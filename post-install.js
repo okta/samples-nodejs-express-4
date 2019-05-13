b1a01b734ae8bee52f086889678e758dc2056a82
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-05-13 12:54:37 -0700

    Adds https check disable flag in config

--- post-install.js
+++ post-install.js
@@ -11,29 +11,4 @@
  */
 
 const createConfigFile = require('./tools/create-config-file');
-
-const sampleConfig = {
-  webServer: {
-    port: 8080,
-    oidc: {
-      clientId: '{clientId}',
-      clientSecret: '{clientSecret}',
-      issuer: 'https://{yourOktaDomain}.com/oauth2/default',
-      appBaseUrl: 'http://localhost:8080',
-      scope: 'openid profile email'
-    },
-  },
-  resourceServer: {
-    port: 8000,
-    oidc: {
-      clientId: '{spaClientId}',
-      issuer: 'https://{yourOktaDomain}.com/oauth2/default'
-    },
-    assertClaims: {
-      aud: 'api://default',
-      cid: '{spaClientId}'
-    }
-  }
-};
-
-createConfigFile(sampleConfig);
+createConfigFile();
