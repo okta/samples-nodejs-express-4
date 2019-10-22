1a2a45f1315cdcddb1a5f30e92644777f4b13ed5
Aaron Granick <aaron.granick@okta.com>
2019-10-22 09:47:28 -0700

    support testenv, log errors

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
-var path = require('path');
+const path = require('path');
+const dotenv = require('dotenv');
+const fs = require('fs');
 
-// Users can also provide the testenv configuration at the root folder: https://www.npmjs.com/package/dotenv
-require('dotenv').config({ path: path.join(__dirname, 'testenv') });
+// Read environment variables from "testenv". Override environment vars if they are already set. https://www.npmjs.com/package/dotenv
+const TESTENV = path.resolve(__dirname, 'testenv');
+if (fs.existsSync(TESTENV)) {
+  const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
+  Object.keys(envConfig).forEach((k) => {
+    process.env[k] = envConfig[k];
+  });
+}
 
 var ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
 var CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
